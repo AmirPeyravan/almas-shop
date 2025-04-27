1571128b0cf29522723fd44ce37e9cff8404deb4
amirpeyravan <amirpayravan>
2025-04-27 20:15:54 +0330

    add-register-and-login

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,11 @@ Route::get('/', function () {
     return view('frontend.master');
 });
 
+
+Route::get('/login', function () {
+    return view('frontend.login');
+});
+
+Route::get('/register', function () {
+    return view('frontend.register');
+});
